87a8a9c3bf014c02bf8b3c5766ddd25d21819bd7
MDung0807 <dominhdung21082002@gmail.com>
2023-05-20 23:52:25 +0700

    create home

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderProj></HeaderProj>
+  <SliderProj value="90"></SliderProj>
+  <img src="./assets/images/study.jpg" alt="Đâny akf ảnh" >
+  <FooterProj></FooterProj>
 </template>
 
 <script>
-
+import HeaderProj from './components/HeaderProj.vue'
+import SliderProj from './components/SlideProj.vue'
+import FooterProj from './components/FooterProj.vue';
 export default {
   name: 'App',
   components: {
-  }
+    HeaderProj,
+    SliderProj,
+    FooterProj
+}
 }
 </script>
 
